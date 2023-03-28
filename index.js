44eb7384a077eba20fe522daa79321b22d65cf3f
Anna <annarsve@gmail.com>
2023-03-28 18:29:19 +0500

    fix cors

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 import express from 'express';
 import multer from 'multer';
 import mongoose from "mongoose";
+import cors from 'cors';
 import { registerValidation, loginValidation } from './validations/validation.js';
-import { checkAuth, handleValidationErrors} from "./utils/index.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
 import { UserController, PostController } from "./constollers/index.js";
 
 import { postCreateValidation } from "./validations/validation.js";
 
+
 mongoose.connect('mongodb+srv://admin:72405060@cluster0.vhuv29j.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
         console.log(('db ok'))
-    }).catch((err) => {console.log('err'+ err)});
+    }).catch((err) => { console.log('err' + err) });
 
 const app = express();
 
 const storage = multer.diskStorage({
-   destination: (_, __, cb) => {
-       cb(null, 'uploads/')
-   },
+    destination: (_, __, cb) => {
+        cb(null, 'uploads/')
+    },
     filename: (_, file, cb) => {
-       cb(null, file.originalname);
+        cb(null, file.originalname);
     }
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 app.use(express.json());
 
@@ -31,7 +33,9 @@ app.get('/', (req, res) => {
     res.send('hallo world');
 });
 
-app.post('/auth/register', registerValidation, handleValidationErrors,  UserController.register);
+app.use(cors());
+
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 
@@ -41,9 +45,9 @@ app.get('/posts', PostController.getAll);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.get('/posts/:id', PostController.getOne);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth,postCreateValidation, handleValidationErrors, PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
-app.post('/upload', checkAuth, upload.single('image'), (req,res) => {
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     res.json({
         url: `/uploads/${req.file.originalname}`
     })
@@ -51,7 +55,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req,res) => {
 
 app.use('/uploads', express.static('uploads'));
 
-app.listen(4444, (err) =>{
+app.listen(4444, (err) => {
     if (err) {
         return console.log(err);
     }
